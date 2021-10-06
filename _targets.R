51f853a5e5574c496bc2ef0ec26418f208e0c66b
Alec L. Robitaille <robit.alec@gmail.com>
2021-10-06 16:46:46 -0700

    copy over stan target

--- _targets.R
+++ _targets.R
@@ -16,3 +16,27 @@ tar_option_set(format = 'qs',
 options(mc.cores = 2,
 				scipen = 999,
 				digits = 2)
+
+
+
+# Data --------------------------------------------------------------------
+
+
+
+
+
+# Targets -----------------------------------------------------------------
+targets_stan <- c(
+	tar_stan_mcmc(
+		stan,
+		stan_files,
+		data = model_data_list,
+		cpp_options = list(stan_threads = TRUE),
+		chains = 4,
+		quiet = FALSE,
+		parallel_chains = 4,
+		threads_per_chain = 4,
+		dir = compiled_dir,
+		output_dir = output_stan_dir
+	)
+)
