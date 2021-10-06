245401f88fe3dfa734310cf7730287c27354a545
Alec L. Robitaille <robit.alec@gmail.com>
2021-10-06 16:34:20 -0700

    more packages

--- R/packages.R
+++ R/packages.R
@@ -7,6 +7,11 @@ library(data.table)
 
 library(rethinking)
 library(tidybayes)
+library(cmdstanr)
+library(posterior)
+library(bayesplot)
+library(boot)
+library(loo)
 
 library(ggdag)
 library(dagitty)
