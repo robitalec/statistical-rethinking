c5d9ce37895bc0cf1ae7214fc8c7ee54601682de
Alec L. Robitaille <robit.alec@gmail.com>
2021-10-07 07:47:17 -0700

    fst data_bangladesh_list

--- R/data_bangladesh.R
+++ R/data_bangladesh.R
@@ -9,3 +9,15 @@ data_bangladesh <- function() {
 	DT[, scale_age := as.numeric(scale(age.centered))]
 
 }
+
+data_bangladesh_list <- function() {
+	DT <- data_bangladesh()
+
+	c(as.list(DT[, .(contraception, district, urban, scale_age,
+									 n_children = living.children)]),
+		N = DT[, .N],
+		K = K,
+		N_district = DT[, uniqueN(district)],
+		alpha_k = list(rep(2L, K))
+	)
+}
