1c89c00546cc2abc3cc1456411d2d431122c0bc5
Alec L. Robitaille <robit.alec@gmail.com>
2021-10-06 17:01:21 -0700

    more bangladesh

--- R/data_bangladesh.R
+++ R/data_bangladesh.R
@@ -1,4 +1,11 @@
 data_bangladesh <- function() {
 	data(bangladesh)
 	DT <- data.table(bangladesh)
+
+	DT[, id := as.integer(woman)]
+	DT[, district := as.integer(as.factor(district))]
+	DT[, contraception := use.contraception]
+
+	DT[, scale_age := as.numeric(scale(age.centered))]
+
 }
