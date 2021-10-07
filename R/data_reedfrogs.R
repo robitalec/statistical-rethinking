0e0cf8cdc7cd2cab64b19548b2bfcc609ce9f76a
Alec L. Robitaille <robit.alec@gmail.com>
2021-10-07 07:34:35 -0700

    fst data_reedfrogs_list

--- R/data_reedfrogs.R
+++ R/data_reedfrogs.R
@@ -2,3 +2,18 @@ data_reedfrogs <- function() {
 	data(reedfrogs)
 	DT <- data.table(reedfrogs)
 }
+
+data_reedfrogs_list <- function() {
+	DT <- data_trolley()
+
+	c(
+		as.list(DT[, .(
+			survival = surv,
+			density,
+			predation = as.integer(pred),
+			size = as.integer(size),
+			tank = seq.int(.N)
+		)]),
+		N = DT[, .N]
+	)
+}
