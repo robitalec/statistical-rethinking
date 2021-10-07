62f38bcd320d7e05256ed284f7e94d2a2308ce0b
Alec L. Robitaille <robit.alec@gmail.com>
2021-10-07 07:25:04 -0700

    data_trolley_list

--- R/data_trolley.R
+++ R/data_trolley.R
@@ -5,3 +5,17 @@ data_trolley <- function() {
 	DT[, education := as.integer(edu_levels[edu])]
 	DT[, individual := as.integer(factor(id))]
 }
+
+data_trolley_list <- function() {
+	DT <- data_trolley()
+
+	c(
+		as.list(DT[, .(response, action, intention, contact, education,
+									 age = as.numeric(scale(age)),
+									 gender = ifelse(male == 1, 0, 1))]),
+		N = DT[, .N],
+		K = DT[, uniqueN(response) - 1],
+		N_edu = DT[, uniqueN(education)],
+		alpha = list(rep(2, 7))
+	)
+}
